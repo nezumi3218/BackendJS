409f9be86bbd111617698cbc19ac1f68ac5602d2
Muskan Yadav <ymuskan3218@gmail.com>
2025-10-11 23:48:49 +0530

    check through postman

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -34,20 +34,24 @@ const registerUser = asyncHandler( async (req, res) => {
         throw new ApiError(409, "User with email or username already exists")
     }
 
-    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const avatarPath = req.files.avatar[0].path;
+    console.log(req.files.avatar[0].path)
+    console.log(avatarPath)
 
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    const coverImageLocalPath = req.files.coverImage[0].path;
 
-    if(!avatarLocalPath) {
+    if( !avatarPath ) {
+        console.log(avatarPath)
         throw new ApiError(400, "Avatar file is required")
+        
     }
 
-    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const avatar = await uploadOnCloudinary(avatarPath)
 
     const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
-    if(!avatar) {
-        throw new ApiError(400, "Avatar file is required") 
+    if( !avatar ) {
+        throw new ApiError(400, "Failed to upload avatar") 
     }
 
     const user = await User.create({
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,5 +1,9 @@
 import {v2 as cloudinary} from "cloudinary"
 import fs from "fs"
+import dotenv from "dotenv"
+
+
+dotenv.config()
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -14,10 +18,11 @@ const uploadOnCloudinary = async (localFilePath) => {
         const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type: "auto"
         })
-        console.log("file is uploaded on cloudinary", response.url);
+        fs.unlinkSync(localFilePath);
         return response
         
     } catch (error) {
+        console.log(error)
         fs.unlinkSync(localFilePath)
         return null
     }
