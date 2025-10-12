c9bac636fd54bb25d8adda31bfdb6b0f05f8caf7
Muskan Yadav <ymuskan3218@gmail.com>
2025-10-12 23:56:21 +0530

    fix all register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -38,7 +38,10 @@ const registerUser = asyncHandler( async (req, res) => {
     console.log(req.files.avatar[0].path)
     console.log(avatarPath)
 
-    const coverImageLocalPath = req.files.coverImage[0].path;
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
 
     if( !avatarPath ) {
         console.log(avatarPath)
