253cf11e36c7ab81bd6a4158812db9907423e70b
Muskan Yadav <ymuskan3218@gmail.com>
2026-01-28 13:39:54 +0530

    feat: import likePost controller in post router

--- src/routes/post.router.js
+++ src/routes/post.router.js
@@ -7,7 +7,7 @@ import {
 } from "../controllers/post.controller.js";
 import { Router } from "express";
 import { upload } from "../middlewares/multer.middleware.js";
-import { likePost } from "../controllers/like.controller.js";
+
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
