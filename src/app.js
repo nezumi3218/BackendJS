e5dfe096d3f8cceaed70fc907e98d379dd812155
Muskan Yadav <156574007+nezumi3218@users.noreply.github.com>
2026-07-13 12:32:54 +0530

    feat: add root endpoint to return status message

--- src/app.js
+++ src/app.js
@@ -17,6 +17,10 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  return res.status(200).json({ message: "Status okkkk!!" });
+});
+
 import userRouter from "./routes/user.routes.js";
 import postRouter from "./routes/post.router.js";
 import likeRouter from "./routes/like.router.js";
